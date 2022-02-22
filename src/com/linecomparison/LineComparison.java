97d2f4ff3d2b04c5b5cd84c0aee2410f6221e3d8
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-02-22 17:07:33 +0530

    Welcome Message

---
+++ src/com/linecomparison/LineComparison.java
@@ -0,0 +1,15 @@
+package com.linecomparison;
+
+public class LineComparison {
+	public void message() {
+		String data = "Welcome to Line Comparison Computation Program";
+		System.out.println(data);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LineComparison lc = new LineComparison();
+		lc.message();
+	}
+
+}
